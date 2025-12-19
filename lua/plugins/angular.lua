51442d2d4725a64770592f2a31508ab65a564e75
DarkSevenX <nargumedor@unicartagena.edu.co>
2025-12-19 11:48:02 -0500

    fix: compatibilidad con angular

--- lua/plugins/angular.lua
+++ lua/plugins/angular.lua
@@ -1,73 +1,80 @@
 return {
+  -- Mason + LSP Config
   {
-    -- Mason + LSP Config
-    {
-      "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup()
-      end,
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = { "angularls", "tailwindcss", "eslint" },
     },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      dependencies = { "williamboman/mason.nvim" },
-      opts = {
-        ensure_installed = { "angularls", "tailwindcss", "eslint" },  -- todo automático
-      },
-    },
-    {
-      "neovim/nvim-lspconfig",
-      dependencies = { "williamboman/mason-lspconfig.nvim" },
-      config = function()
-        local lspconfig = require("lspconfig")
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      -- 1. Definimos las capacidades para nvim-cmp (necesario para el autocompletado)
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-        lspconfig.angularls.setup({
-          -- ¡Con esto solo ya funciona al 100% en el 95% de proyectos!
-        })
+      -- 2. Configuramos los servidores usando la nueva API nativa
+      -- En lugar de lspconfig.server.setup({}), usamos vim.lsp.config
+      
+      vim.lsp.config('angularls', {
+        capabilities = capabilities,
+        -- Añade aquí cualquier configuración específica de angularls si la necesitas
+      })
 
-        -- Opcional pero recomendado: tailwind y eslint
-        lspconfig.tailwindcss.setup({})
-        lspconfig.eslint.setup({})
-      end,
-    },
+      vim.lsp.config('tailwindcss', { capabilities = capabilities })
+      vim.lsp.config('eslint', { capabilities = capabilities })
 
-    -- Autocompletado brutal
-    {
-      "hrsh7th/nvim-cmp",
-      dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-      },
-      config = function()
-        local cmp = require("cmp")
-        cmp.setup({
-          snippet = { expand = function(args) require("luasnip").lsp_expand(args.body) end },
-          mapping = cmp.mapping.preset.insert({
-            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          }),
-          sources = {
-            { name = "nvim_lsp" },
-            { name = "luasnip" },
-            { name = "buffer" },
-            { name = "path" },
-          },
-        })
-      end,
-    },
+      -- 3. Habilitamos los servidores
+      vim.lsp.enable('angularls')
+      vim.lsp.enable('tailwindcss')
+      vim.lsp.enable('eslint')
+    end,
+  },
 
-    -- Navegación rápida entre template/component/style (esto es oro puro)
-    {
-      "joeveiga/ng.nvim",
-      keys = {
-        { "<leader>gt", "<cmd>NgGotoTemplate<cr>", desc = "Goto Template" },
-        { "<leader>gc", "<cmd>NgGotoComponent<cr>", desc = "Goto Component" },
-        { "<leader>gs", "<cmd>NgGotoStyle<cr>", desc = "Goto Style" },
-      },
+  -- Autocompletado (Se mantiene igual, funciona perfectamente)
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
     },
-  }
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = { expand = function(args) require("luasnip").lsp_expand(args.body) end },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+
+  -- Navegación Angular
+  {
+    "joeveiga/ng.nvim",
+    keys = {
+      { "<leader>gt", "<cmd>NgGotoTemplate<cr>", desc = "Goto Template" },
+      { "<leader>gc", "<cmd>NgGotoComponent<cr>", desc = "Goto Component" },
+      { "<leader>gs", "<cmd>NgGotoStyle<cr>", desc = "Goto Style" },
+    },
+  },
 }
